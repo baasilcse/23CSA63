2e07dd5e54fe76a49eda9f5b926dc147d0485085
Mohamed Baasil <mbaasil28@gmail.com>
2026-06-30 12:04:13 +0530

    logging-middleware completed

---
+++ logging-middleware/Logging-middleware.jsx
@@ -0,0 +1,10 @@
+function log(_stack,_level,_package,_message){
+    let stack =["backend" ,"frontend"]
+    let level=["debug","info","warn","error","fatal"]
+    let package =["api","component","hook","page","state","style","auth","config","middleware","utils"]
+    let message =_message;
+
+
+    
+}
+
